1fcf5207b93b153eafd75643c89cb28a1a6db135
wns8628 <wns8628>
2019-01-04 09:27:59 +0900

    제너릭&가변변수

--- src/prob01/Printer.java
+++ src/prob01/Printer.java
@@ -3,17 +3,41 @@ package prob01;
 public class Printer{
 	public Printer(){}
 	
-	public void println(int age) {
-		System.out.println(age);
-	}
-	public void println(boolean yesorno) {
-		System.out.println(yesorno);
-	}
-	public void println(double weight) {
-		System.out.println(weight);
-	}
-	public void println(String name) {
-		System.out.println(name);
+//	public void println(int age) {
+//		System.out.println(age);
+//	}
+//	public void println(boolean yesorno) {
+//		System.out.println(yesorno);
+//	}
+//	public void println(double weight) {
+//		System.out.println(weight);
+//	}
+//	public void println(String name) {
+//		System.out.println(name);
+//	}
+	
+	//제너릭쓰면됨
+//	public <T> void println(T t) {
+//		System.out.println(t);
+//	}
+	
+	//쓸떄 sum(1,2,3,4,5)
+	//쓸떄 sum(1,2) 이런식으로쓰고싶다 
+	//가변변수 
+	public Integer sum(Integer... nums) {
+		Integer sum = 0;			
+		for(Integer i:nums) {
+			sum +=i;
+		}
+		return sum;
 	}
 	
+	//가변변수 + 제너릭 
+	public <T> void println(T... ts) {
+		for(T t : ts) {
+			System.out.println(t);	
+		}
+	}
+	
+	 
 }
--- src/prob01/PrinterTest.java
+++ src/prob01/PrinterTest.java
@@ -8,9 +8,19 @@ public class PrinterTest {
 		printer.println( 10 );
 		printer.println( true );
 		printer.println( 5.7 );
-		printer.println( "홍길동" );
+		printer.println( "홍길동" );	
 		
 		//메소드제너릭? 오버로딩문제임 걍 오버로딩으로푸셈
+		//제너릭완료
+		
+		//가변변수
+		System.out.println(printer.sum(1,2,3));
+		System.out.println(printer.sum(1,2,3,4,5));
+		
+		
+		printer.println( 10,true, 5.7,"홍길동" );	
+		
+		
 	}
 
 }
